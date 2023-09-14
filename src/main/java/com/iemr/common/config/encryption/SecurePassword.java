6ca71b89d023ef4bcbd17bbd3f9a4fd3f36040ec
Aditi Salokhe <AD20362721@wipro.com>
2023-09-14 12:40:06 +0530

    Use of a broken or risky cryptographic algorithm in SecurePasswoed

--- src/main/java/com/iemr/common/config/encryption/SecurePassword.java
+++ src/main/java/com/iemr/common/config/encryption/SecurePassword.java
@@ -72,7 +72,7 @@ public class SecurePassword {
 		byte[] hash = fromHex(parts[2]);
 		if (iterations == 1000) {
 			PBEKeySpec spec = new PBEKeySpec(originalPassword.toCharArray(), salt, 1000, hash.length * 8);
-			SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+			SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
 			byte[] testHash = skf.generateSecret(spec).getEncoded();
 			int diff = hash.length ^ testHash.length;
 			for (int i = 0; (i < hash.length) && (i < testHash.length); i++) {
